6d24190e37f0d52720282707b37f657f2c58bb46
navi9000 <icdi7434@gmail.com>
2026-01-19 12:52:35 +0300

    test workflow

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import Test from "./Test"
 
+throw new Error("Test workflow")
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Test />
