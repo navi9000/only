237e9d1d52ef4750fba57244d752d9b05d1f26a4
navi9000 <icdi7434@gmail.com>
2026-01-20 23:33:14 +0300

    possibly fix fonts

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      // preload: "*.ttf",
+      preload: "*.ttf",
     }),
     new ProvidePlugin({
       React: "react",
